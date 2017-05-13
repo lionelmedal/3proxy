bbc7eb66cd043db9b1ff0be5842f871e0c473744
liomedal <cantcheckout@gmail.com>
2017-05-13 09:07:22 -0400

    Create 3proxyuninst.sh

---
+++ 3proxyuninst.sh
@@ -0,0 +1,10 @@
+/etc/init.d/3proxyinit stop
+rm -rf /etc/3proxy
+rm /etc/rc0.d/*proxyinit
+rm /etc/rc1.d/*proxyinit 
+rm /etc/rc6.d/*proxyinit
+rm /etc/rc2.d/*proxyinit
+rm /etc/rc3.d/*proxyinit 
+rm /etc/rc4.d/*proxyinit
+rm /etc/rc5.d/*proxyinit
+rm /etc/init.d/3proxyinit
