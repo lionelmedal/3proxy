4a346a086f0fa7595d355516e0c3bf52828d7c1c
Lionel Medal <cantcheckout@gmail.com>
2018-01-17 09:15:51 -0500

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -1,6 +1,6 @@
 apt-get update && apt-get -y upgrade
 apt-get install -y build-essential nano
-wget --no-check-certificate https://github.com/liomedal/3proxy/blob/master/archive/3proxy-3proxy-0.8.6.tar.gz
+wget --no-check-certificate https://github.com/lionelmedal/3proxy/blob/master/archive/3proxy-3proxy-0.8.6.tar.gz
 tar xzf 3proxy-0.8.6.tar.gz
 cd 3proxy-3proxy-0.8.6
 make -f Makefile.Linux
@@ -8,11 +8,11 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/liomedal/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/lionelmedal/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
-wget --no-check-certificate https://github.com/liomedal/3proxy/raw/master/.proxyauth
+wget --no-check-certificate https://github.com/lionelmedal/3proxy/raw/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate https://raw.github.com/liomedal/3proxy/master/3proxyinit
+wget --no-check-certificate https://raw.github.com/lionelmedal/3proxy/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 update-rc.d 3proxyinit defaults
